ea69deaed364a48acfc971a414ed39f2209c7a7f
FreeWebUi <freewebui@gmail.com>
2023-11-30 15:42:20 +0530

    initial commit

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,9 @@
+import Container from "../container/Container";
+
+export default function Header() {
+    return <div className="header">
+        <Container>
+            <div>Header Section</div>
+        </Container>
+    </div>
+}
